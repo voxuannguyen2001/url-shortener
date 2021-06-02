3c0b3c87056b00914e0731c2bb2d0b2fe4a1ee9c
Nguyen Vo <voxuannguyen2001@gmail.com>
2021-06-02 23:34:08 +0700

    changed port to process.env.PORT

--- index.js
+++ index.js
@@ -15,6 +15,5 @@ app.use('/', express.static(__dirname + '/static'));
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
 
-const PORT = 5000;
 
-app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)});
+app.listen(process.env.PORT || 5000, () => {console.log(`Server running on port ${PORT}`)});
